e28ae1a0111d10787e1c4a72210f86f553182231
Nischal Bhattarai <nishchalb21@gmail.com>
2024-07-20 22:00:35 +0545

    bundler

--- Namaste-React/ignite-app/react1.js
+++ Namaste-React/ignite-app/react1.js
@@ -1,3 +1,7 @@
+ import React from "react";
+ import ReactDOM from "react-dom";
+ 
+ 
  const heading = React.createElement("div",{ id:"parent"},
     [React.createElement("div",{ id:"child"},
         [React.createElement("h3",{},"hello world from react"),
